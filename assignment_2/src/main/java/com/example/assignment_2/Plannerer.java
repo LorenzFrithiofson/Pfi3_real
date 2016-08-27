3a1bce74c2b7b3ca6b70bfb2c15db5bfd80ec922
Lorenz <lorenzzz_165@hotmail.com>
2016-08-27 16:04:24 +0200

    assignment2

---
+++ assignment_2/src/main/java/com/example/assignment_2/Plannerer.java
@@ -0,0 +1,24 @@
+package com.example.assignment_2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Lorenz on 2016-08-27.
+ */
+public class Plannerer extends Fragment {
+    public Plannerer() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_plannerer, container, false);
+    }
+}
---
+++ assignment_2/src/main/java/com/example/assignment_2/Start.java
@@ -0,0 +1,25 @@
+package com.example.assignment_2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Lorenz on 2016-08-27.
+ */
+public class Start extends Fragment {
+    public Start() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false);
+
+    }
+}
